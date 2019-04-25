ae5899b85c40861fff908e9485fa89663807b360
Franklin Timóteo <franklintimoteo@live.com>
2019-04-24 22:55:22 -0300

    Adiçao background. Carregamento de dados atraves do carregamento do body automaticamente.

--- app.js
+++ app.js
@@ -35,6 +35,24 @@ class BD{
         localStorage.setItem('id', id)
         
     }
+
+    recuperarTodosRegistros(){
+        let despesas = Array()
+
+        let id = localStorage.getItem('id')
+        
+        for(let i = 1; i<= id; i++){
+            let despesa = localStorage.getItem(i)
+            despesa = JSON.parse(despesa)
+
+            if (despesa === null){
+                continue
+            }
+            despesas.push(despesa)
+        }
+
+        return despesas
+    }
 }
 
 let bd = new BD()
@@ -63,4 +81,27 @@ function cadastrarDispesa(){
         document.getElementById('dados-validos').style.display = 'block'
         bd.gravar(despesa)
     }
+}
+
+function carregaListaDespesas(){
+    let despesas = Array()
+    despesas = bd.recuperarTodosRegistros()
+
+    let listaDespesas = document.getElementById("listaDespesas")
+    
+    despesas.forEach(function(d){
+        let linha = listaDespesas.insertRow()
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+        
+        switch(parseInt(d.tipo)){
+            case 1: d.tipo = "Alimentação"; break
+            case 2: d.tipo = "Educação"; break
+            case 3: d.tipo = "Lazer"; break
+            case 4: d.tipo = "Saúde"; break
+            case 5: d.tipo = "Transporte"; break
+        }
+        linha.insertCell(1).innerHTML = d.tipo
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = d.valor
+    })
 }
